22e6551d917345091250e0a00d17737567a76454
uXiango <k23589612@gmail.com>
2021-06-24 17:52:50 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //        System.out.println("Hello World!");
         Person p = new Person();
         p.hello();
+
+        int age = 19;
+        Integer age2 = 19;
+        System.out.println(age2.equals(19));
+        char a = 'A';
+        float f = 19.7f;
+        byte b = 120;
+        boolean adult = true;
+        String name = "Apple";
+
+
     }
 }
--- src/main/kotlin/com/tom/kotlin/Hello.kt
+++ src/main/kotlin/com/tom/kotlin/Hello.kt
@@ -4,6 +4,9 @@ fun main() {
 //    println("Hello World!")
     val h = Human()
     h.hello()
+    val age : Int = 19; //val 表示固定不可改變
+    var age2 = 20; // var可以被改變
+    age2 = 21;
 }
 
 class Human {
